2eb5c5f5d6f9fa3369da6305f004bf12810a49b1
hsingarajah <hsingarajah@gmail.com>
2024-04-05 20:32:50 -0400

    Exracted layer 1 of exif data

---
+++ MyPhoto.py
@@ -0,0 +1,3 @@
+from PIL import Image
+class Myphoto(Object):
+    My
--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import os
-
-def printDir(dir):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Current working directory, {dir}')  # Press ⌘F8 to toggle the breakpoint.
-
+from PIL import Image
+from PIL.ExifTags import GPS
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     myDir = os.getcwd()
-    printDir(myDir)
+    PhotoDir = 'MyPhotos'
+    PDFDir = 'MyPDFs'
+    PDFname = 'testoutput'
+
+    ImageArray = []
+    ExifArray = []
+    files = (file for file in os.listdir(myDir + '/'+ PhotoDir)
+             if os.path.isfile(os.path.join(myDir + '/'+ PhotoDir, file)))
+    for file in files:  # You could shorten this to one line, but it runs on a bit.
+        if file.endswith ('.JPG') or file.endswith('.jpg'):
+            print(myDir + '/' + PhotoDir + '/' + file)
+            ImageArray.append(Image.open(myDir + '/' + '/' + PhotoDir + '/' + file))
+
+    print (len(ImageArray))
+
+    for i in ImageArray:
+         ExifArray.append( i.getexif())
+
+    for e in ExifArray:
+        print("my pic")
+        for k, v in e.items():
+            print("Tag", hex(k), "Value", v)  # Tag 274 Value 2
+    pdf_path = myDir + '/' + PDFDir + '/' + PDFname + '.pdf'
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
